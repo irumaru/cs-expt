95a8f0f16dd94d35557ae475a01487524ff04b09
irumaru <jibakomaorochi13@outlook.jp>
2024-08-27 05:38:07 +0000

    Add generics

---
+++ generics/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static Type Max<Type>(Type a, Type b)
+  where Type : IComparable
+{
+  return a.CompareTo(b) > 0 ? a : b;
+}
+
+Console.WriteLine(Max(5, 10));
+Console.WriteLine(Max(10.2, 3.1));
