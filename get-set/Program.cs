26ced015b66ad997a6dab40dfae1d136c6c0bbfd
irumaru <jibakomaorochi13@outlook.jp>
2024-08-30 05:28:29 +0000

    Add get-set

---
+++ get-set/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var person = new Person { Name = "Alice", Age = 25 };
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+    }
+}
