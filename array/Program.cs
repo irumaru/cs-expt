05a76e5bdf8faa539eb08adfc16a369b34548b5c
irumaru <jibakomaorochi13@outlook.jp>
2024-08-30 14:56:52 +0000

    Add array

---
+++ array/Program.cs
@@ -0,0 +1,11 @@
+﻿
+float[] weights = new float[3];
+
+weights[0] = 68.5f;
+weights[1] = 70.3f;
+weights[2] = 65.2f;
+
+for (int i = 0; i < weights.Length; i++)
+{
+  Console.WriteLine(weights[i]);
+}
